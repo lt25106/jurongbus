ab09bf285eb68e83f8f586e7a636090faf313cf5
name <11pro12ispro@gmail.com>
2025-07-20 12:06:18 +0800

    more unreadable but eh

--- j.js
+++ j.js
@@ -43,8 +43,8 @@ const busdata = await fetch("https://arrivelah2.busrouter.sg/?id=28009")
     if ([334,506,66,993].includes(Number(element.no))) {
       tbody.innerHTML += `
       <td>${element.no}</td>
-      <td>${element.next && element.next.time ? element.next.time.slice(11,16) : ""}</td>
-      <td>${element.next2 && element.next2.time ? element.next2.time.slice(11,16) : ""}</td>
+      <td>${element.next?.time ? element.next.time.slice(11,16) : ""}</td>
+      <td>${element.next2?.time ? element.next2.time.slice(11,16) : ""}</td>
       `
     }
   });
