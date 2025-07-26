5c5b845467020debfba7afe4aa794320aaba3ba6
name <11pro12ispro@gmail.com>
2025-07-26 20:32:06 +0800

    just move it in lil bro

--- j.js
+++ j.js
@@ -1,20 +1,13 @@
 const tbody = document.querySelector("tbody");
-
-/**
- * @typedef {"SEA" | "SDA" | "LSD"} LoadLevel - Passenger load levels
- * @typedef {"WAB"} BusFeature - Features like wheelchair access
- * @typedef {"SD" | "DD"} BusType - Bus types: Single Deck (SD), Double Deck (DD)
- */
-
 /**
  * @typedef {Object} BusTiming
  * @property {string} time - ISO timestamp when the bus is expected
  * @property {number} duration_ms - Duration in milliseconds until arrival
  * @property {number} lat - Latitude (currently 0)
  * @property {number} lng - Longitude (currently 0)
- * @property {LoadLevel} load - Load level (e.g., SEA)
- * @property {BusFeature} feature - Bus features (e.g., WAB)
- * @property {BusType} type - Type of the bus (e.g., SD, DD)
+ * @property {"SEA" | "SDA" | "LSD"} load - Load level (e.g., SEA)
+ * @property {"WAB"} feature - Bus features (e.g., WAB)
+ * @property {"SD" | "DD"} type - Type of the bus (e.g., SD, DD)
  * @property {number} visit_number - The sequence number of this visit
  * @property {string} origin_code - Bus stop code of origin
  * @property {string} destination_code - Bus stop code of destination
