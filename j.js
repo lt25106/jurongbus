1e933c0f4b333d309ce8bece9a495787f81776c6
name <11pro12ispro@gmail.com>
2025-07-28 09:56:52 +0800

    typescript

--- j.js
+++ j.js
@@ -1,47 +1,19 @@
-const tbody = document.querySelector("tbody");
-/**
- * @typedef {Object} BusTiming
- * @property {string} time - ISO timestamp when the bus is expected
- * @property {number} duration_ms - Duration in milliseconds until arrival
- * @property {number} lat - Latitude (currently 0)
- * @property {number} lng - Longitude (currently 0)
- * @property {"SEA" | "SDA" | "LSD"} load - Load level (e.g., SEA)
- * @property {"WAB"} feature - Bus features (e.g., WAB)
- * @property {"SD" | "DD"} type - Type of the bus (e.g., SD, DD)
- * @property {number} visit_number - The sequence number of this visit
- * @property {string} origin_code - Bus stop code of origin
- * @property {string} destination_code - Bus stop code of destination
- * @property {0 | 1} monitored - Whether the bus is monitored
- */
-
-/**
- * @typedef {Object} BusService
- * @property {string} no - Bus service number (e.g., "105", "143M")
- * @property {string} operator - Bus operator code (e.g., "SBST", "TTS")
- * @property {BusTiming} next - Information on the next bus
- * @property {BusTiming} subsequent - Information on the subsequent bus
- * @property {BusTiming} next2 - Same as `subsequent`, may be a duplicate
- * @property {BusTiming} next3 - Information on the third bus (may be null)
- */
-
-/**
- * @typedef {Object} BusData
- * @property {BusService[]} services - Array of bus service data
- */
-const busdata = await fetch("https://arrivelah2.busrouter.sg/?id=28009")
-.then(response => response.json())
-.then(data => {
-  console.log(data);
-  data.services.forEach(element => {
-    if ([334,506,66,993].includes(Number(element.no))) {
-      tbody.innerHTML += `
+const tblebdy = document.querySelector("tbody");
+await fetch("https://arrivelah2.busrouter.sg/?id=28009")
+    .then(response => response.json())
+    .then((data) => {
+    console.log(data);
+    data.services.forEach(element => {
+        if ([334, 506, 66, 993].includes(Number(element.no))) {
+            tblebdy.innerHTML += `
       <tr>
         <td>${element.no}</td>
-        <td>${element.next?.time ? element.next.time.slice(11,16) : ""}</td>
-        <td>${element.next2?.time ? element.next2.time.slice(11,16) : ""}</td>
-        <td>${element.next3?.time ? element.next3.time.slice(11,16) : ""}</td>
+        <td>${element.next?.time ? element.next.time.slice(11, 16) : ""}</td>
+        <td>${element.next2?.time ? element.next2.time.slice(11, 16) : ""}</td>
+        <td>${element.next3?.time ? element.next3.time.slice(11, 16) : ""}</td>
       </tr>
-      `
-    }
-  });
-})
+      `;
+        }
+    });
+});
+export {};
---
+++ j.ts
@@ -0,0 +1,44 @@
+interface Busdata {
+  services: Array[];
+}
+interface Array {
+  no: number;
+  operator: string;
+  next?: Next;
+  next2?: Next;
+  next3?: Next;
+}
+interface Next {
+  destination_code: string;
+  duration_ms: number;
+  feature: string;
+  lat: number;
+  lng: number;
+  load: string;
+  monitored: 0 | 1;
+  origin_code: string;
+  time: string; // ISO 8601 format
+  type: string;
+  visit_number: number;
+}
+
+const tblebdy = document.querySelector("tbody") as HTMLTableSectionElement;
+await fetch("https://arrivelah2.busrouter.sg/?id=28009")
+.then(response => response.json())
+.then((data:Busdata) => {
+  console.log(data);
+  data.services.forEach(element => {
+    if ([334,506,66,993].includes(Number(element.no))) {
+      tblebdy.innerHTML += `
+      <tr>
+        <td>${element.no}</td>
+        <td>${element.next?.time ? element.next.time.slice(11,16) : ""}</td>
+        <td>${element.next2?.time ? element.next2.time.slice(11,16) : ""}</td>
+        <td>${element.next3?.time ? element.next3.time.slice(11,16) : ""}</td>
+      </tr>
+      `
+    }
+  });
+})
+
+export {};
