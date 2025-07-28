e9e8bf7356576d67c3e1839e12b0abc8e72180ed
name <11pro12ispro@gmail.com>
2025-07-28 15:45:03 +0800

    another station

--- j.js
+++ j.js
@@ -1,11 +1,28 @@
-const tblebdy = document.querySelector("tbody");
+const intable = document.getElementById("int");
 await fetch("https://arrivelah2.busrouter.sg/?id=28009")
     .then(response => response.json())
     .then((data) => {
-    console.log(data);
+    // console.log(data);
     data.services.forEach(element => {
         if ([334, 506, 66, 993].includes(+element.no)) {
-            tblebdy.innerHTML += `
+            intable.innerHTML += `
+      <tr>
+        <td>${element.no}</td>
+        <td>${element.next?.time ? element.next.time.slice(11, 16) : ""}</td>
+        <td>${element.next2?.time ? element.next2.time.slice(11, 16) : ""}</td>
+        <td>${element.next3?.time ? element.next3.time.slice(11, 16) : ""}</td>
+      </tr>
+      `;
+        }
+    });
+});
+const libtable = document.getElementById("lib");
+await fetch("https://arrivelah2.busrouter.sg/?id=28241")
+    .then(response => response.json())
+    .then((data) => {
+    data.services.forEach(element => {
+        if ([178, 198].includes(+element.no)) {
+            libtable.innerHTML += `
       <tr>
         <td>${element.no}</td>
         <td>${element.next?.time ? element.next.time.slice(11, 16) : ""}</td>
--- j.ts
+++ j.ts
@@ -21,14 +21,14 @@ interface Next {
   visit_number: number;
 }
 
-const tblebdy = document.querySelector("tbody") as HTMLTableSectionElement;
+const intable = document.getElementById("int") as HTMLTableSectionElement;
 await fetch("https://arrivelah2.busrouter.sg/?id=28009")
 .then(response => response.json())
 .then((data:Busdata) => {
-  console.log(data);
+  // console.log(data);
   data.services.forEach(element => {
     if ([334,506,66,993].includes(+element.no)) {
-      tblebdy.innerHTML += `
+      intable.innerHTML += `
       <tr>
         <td>${element.no}</td>
         <td>${element.next?.time ? element.next.time.slice(11,16) : ""}</td>
@@ -38,6 +38,24 @@ await fetch("https://arrivelah2.busrouter.sg/?id=28009")
       `
     }
   });
+})
+
+const libtable = document.getElementById("lib") as HTMLTableSectionElement;
+await fetch("https://arrivelah2.busrouter.sg/?id=28241")
+.then(response => response.json())
+.then((data:Busdata) => {
+  data.services.forEach(element => {
+    if ([178,198].includes(+element.no)) {
+      libtable.innerHTML += `
+      <tr>
+        <td>${element.no}</td>
+        <td>${element.next?.time ? element.next.time.slice(11,16) : ""}</td>
+        <td>${element.next2?.time ? element.next2.time.slice(11,16) : ""}</td>
+        <td>${element.next3?.time ? element.next3.time.slice(11,16) : ""}</td>
+      </tr>
+      `
+    }
+  })
 })
 
 export {};
