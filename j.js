f06c9a6a125f5eb28a690d0f6d2a00afba8cec31
name <11pro12ispro@gmail.com>
2025-08-01 18:26:15 +0800

    might work

--- j.js
+++ j.js
@@ -1,11 +1,11 @@
-var e=document.getElementById("int");await fetch("https://arrivelah2.busrouter.sg/?id=28009").then(i=>i.json()).then(i=>{i.services.forEach(t=>{[334,506,66,993].includes(+t.no)&&(e.innerHTML+=`
+const e=document.getElementById("int");await fetch("https://arrivelah2.busrouter.sg/?id=28009").then(i=>i.json()).then(i=>{i.services.forEach(t=>{[334,506,66,993].includes(+t.no)&&(e.innerHTML+=`
       <tr>
         <td>${t.no}</td>
         <td>${t.next?.time?t.next.time.slice(11,16):""}</td>
         <td>${t.next2?.time?t.next2.time.slice(11,16):""}</td>
         <td>${t.next3?.time?t.next3.time.slice(11,16):""}</td>
       </tr>
-      `)})});var n=document.getElementById("lib");await fetch("https://arrivelah2.busrouter.sg/?id=28241").then(i=>i.json()).then(i=>{i.services.forEach(t=>{[178,198].includes(+t.no)&&(n.innerHTML+=`
+      `)})});const n=document.getElementById("lib");await fetch("https://arrivelah2.busrouter.sg/?id=28241").then(i=>i.json()).then(i=>{i.services.forEach(t=>{[178,198].includes(+t.no)&&(n.innerHTML+=`
       <tr>
         <td>${t.no}</td>
         <td>${t.next?.time?t.next.time.slice(11,16):""}</td>
